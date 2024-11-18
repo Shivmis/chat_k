dfc9c98e2752bfcae24f4c0085ac35152f3b6a95
Shivmis <githubshiv@gmail.com>
2024-11-18 11:43:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "24280518"
+API_ID = "24946423"
 # -------------------------------------------------------------
-API_HASH = "ec5793b4bef624381b19ad2b6fd082f8"
+API_HASH = "30ba9fa7064625f3d952b8adb75f636c"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "1808943146"))
-SUPPORT_GRP = "Meetupzone_Chatting"
-UPDATE_CHNL = "Ur_karma_baby"
-OWNER_USERNAME = "introvertt_i"
+OWNER_ID = int(getenv("OWNER_ID", "7491834397"))
+SUPPORT_GRP = "FORENSICXWORLD"
+UPDATE_CHNL = "II_MOON_WORLD_XD"
+OWNER_USERNAME = "Alexu_xd"
